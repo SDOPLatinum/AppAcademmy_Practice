dcb38c521d33aeb96dc3d4e0ff8dda0d5c49a79d
SDOPLatinum <33947535+SDOPLatinum@users.noreply.github.com>
2017-11-25 15:20:42 -0500

    Update 10-third-greatest.rb

--- 10-third-greatest.rb
+++ 10-third-greatest.rb
@@ -8,16 +8,15 @@ def third_greatest(nums)
   
   stable=0
   moving=0
-  difference=nums[stable]-nums[moving]
   count=0
   
   while moving<nums.length
     
-    if difference<0
+    if nums[stable]-nums[moving]<0
       count+=1
     end
     
-    if count==2
+    if count==2 && moving==nums.length-1
       return nums[stable]
     else
       moving+=1
@@ -27,10 +26,6 @@ def third_greatest(nums)
       count=0
       moving=0
       stable+=1
-    end
-    
-    if stable==nums.length
-      return "This number array does not have a thrid greatest number."
     end
     
   end
@@ -51,4 +46,4 @@ puts(
 puts(
   'third_greatest([2, 3, 7, 4]) == 3: ' +
   (third_greatest([2, 3, 7, 4]) == 3).to_s
-)
+  )
