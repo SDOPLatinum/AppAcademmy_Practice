e99ed7b47590c5a8a4e685c95548cf2e2acb993e
SDOPLatinum <33947535+SDOPLatinum@users.noreply.github.com>
2017-11-25 10:59:29 -0500

    Update 08-two-sum.rb

--- 08-two-sum.rb
+++ 08-two-sum.rb
@@ -5,6 +5,31 @@
 # Difficulty: medium.
 
 def two_sum(nums)
+  
+  partner1=0
+  partner2=0
+  summation=parnter1+partner2
+  output=[partner1, partner2]
+  
+  while partner2<nums.length
+    
+    if summation==0
+      return output
+    else
+      partner2+=1
+    end
+    
+    if partner2==nums.length-1 && partner1<nums.length-1
+      partner2=0
+      partner1+=1
+    else 
+      partner2+=1
+    end
+    
+  end
+  
+  return output
+  
 end
 
 # These are tests to check that your code is working. After writing
