eea8bb8df41d99ed76d93bfefc407b400db5b88e
SDOPLatinum <33947535+SDOPLatinum@users.noreply.github.com>
2017-11-25 15:42:48 -0500

    Update 11-most-common-letter.rb

--- 11-most-common-letter.rb
+++ 11-most-common-letter.rb
@@ -5,6 +5,38 @@
 # Difficulty: medium.
 
 def most_common_letter(string)
+  
+  candidate=0
+  tester=0
+  count=0
+  best_count=0
+  best_letter=0
+  
+  while tester<string.length
+    
+    if string[candidate]==string[tester]
+      count+=1
+    end
+    
+    tester+=1
+    
+    if tester==string.length && candidate<string.length-1
+      
+      if count>best_count
+        best_count=count
+        best_letter=candidate
+      end
+        
+      tester=0
+      candidate+=1
+      count=0
+      
+    end
+    
+  end
+  
+  return [string[best_letter], best_count]
+  
 end
 
 # These are tests to check that your code is working. After writing
